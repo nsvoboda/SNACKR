b39411c613dc780784b423754e8a4155231baec5
saraneuie <sneuensc@umich.edu>
2022-01-19 13:32:09 -0500

    fixing some html issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ $(function () {
     // Spoonacular URL with the variables holding the key and user input appened
    // var spoonApiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=spoonApiKey+"&ingredients="+ TEST_ONLY_VAR +"&number=5"`
     
-    var fetchSpoonData = function (ingredients) {
-        let spoonApiUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=4eb1b45b7edb4a658a39c27390eaebe2&${ingredients}'
+    var fetchSpoonData = function (options) {
+        let spoonApiUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=4eb1b45b7edb4a658a39c27390eaebe2&ingredients=options'
 
         fetch(spoonApiUrl).then(function(response){
             console.log(response);

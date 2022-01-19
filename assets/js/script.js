6970809e4c7de9400029bca8dddd8f26d1ae3183
saraneuie <sneuensc@umich.edu>
2022-01-19 17:30:58 -0500

    still working on the display

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,29 @@ $(function () {
     var fetchSpoonData = function (ingredients) {
         let spoonApiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=4eb1b45b7edb4a658a39c27390eaebe2&ingredients=${ingredients}`
 
-        fetch(spoonApiUrl).then(function(response){
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-            })
-        });
+        fetch(spoonApiUrl)
+            .then(data => data.json())
+            .then(data => showSearchHistory(data));
     }
 
-    
+    var showSearchHistory = function (recipeResults) {
+        var recipeList = "";
+        for (var i = 0; i > recipeResults.length; i++) {
+            recipeList = `<li> ${recipeResults[i]} </li>`
+        }
+        ingredients.innerHTML = recipeList;
+    };
 
     var displayResults = function(recipeResults) {
         recipeResults.title
         recipeResults.image
-    }
 
     $("#burritos").on("click", function(event){
         event.preventDefault();
         //grab the text
         var searchText = $("#ingredient-search").val().trim();
         fetchSpoonData(searchText);
-        displayResults()
+        showSearchHistory();
     })
 
 

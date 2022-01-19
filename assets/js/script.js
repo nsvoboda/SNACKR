004dc6d6d0ce2223e92501b277cdd17250669a37
saraneuie <sneuensc@umich.edu>
2022-01-18 21:00:19 -0500

    lonely m

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,29 @@ $(function () {
     // Spoonacular API Key
     var spoonApiKey = "4eb1b45b7edb4a658a39c27390eaebe2"
     // Spoonacular URL with the variables holding the key and user input appened
-    var spoonApiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=spoonApiKey+"&ingredients="+ TEST_ONLY_VAR +"&number=5"`
+   // var spoonApiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=spoonApiKey+"&ingredients="+ TEST_ONLY_VAR +"&number=5"`
+    
+    var fetchSpoonData = function (ingredients) {
+        let spoonApiUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=4eb1b45b7edb4a658a39c27390eaebe2&${ingredients}'
 
-
-    var fetchSpoonData = function(){
         fetch(spoonApiUrl).then(function(response){
             console.log(response);
-            if (response.ok){
-                response.json().then(function(data){
-                    console.log(data);
-                })
-            }
+            response.json().then(function(data) {
+                console.log(data);
+            })
         });
     }
 
-    fetchSpoonData();
+    $("#burritos").on("click", function(event){
+        event.preventDefault();
+        //grab the text
+        var searchText = $("#search-box").val().trim();
+        fetchSpoonData(searchText);
+    })
+
+
+
+
+
 
 });
